0dbd1b4cc50486c8008292102888ab44b7d59251
Lostlight <robpirojk@gmail.com>
2020-04-29 12:52:32 +0300

    RK 1, var 2, task 1

--- rk_1/var2_n1/main.c
+++ rk_1/var2_n1/main.c
@@ -1,33 +1,33 @@
 #define __USE_MINGW_ANSI_STDIO 1
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define NMAX 64
 
-long input_array(long *a, size_t n);
+long input_array(long a[NMAX], size_t n);
 void output_array(long *a, size_t n);
-double arithmetic_avg_to_n(long *a, size_t n);
-size_t delete_by_avg(long *a, size_t n, double avg);
-void delete_by_i(long *a, size_t *n, size_t *i);
+long abs_sum(long a[NMAX], size_t n);
+long delete_by_sum(long a[NMAX], size_t n, long sum);
+void delete_by_i(long a[NMAX], size_t *n, size_t *i);
 
 int main(void)
 {
     long a[NMAX];
     size_t n, p;
-    double avg;
+    long sum;
 
     if (scanf("%zu", &n) != 1 || n == 0 || n > 64)
         return EXIT_FAILURE;
-
     if (scanf("%zu", &p) != 1 || p > n)
         return EXIT_FAILURE;
 
     if (input_array(a, n))
         return EXIT_FAILURE;
 
-    avg = arithmetic_avg_to_n(a, p);
+    sum = abs_sum(a, p);
 
-    n = delete_by_avg(a, n, avg);
+    n = delete_by_sum(a, n, sum);
 
     output_array(a, n);
 
@@ -51,22 +51,22 @@ void output_array(long *a, size_t n)
     return;
 }
 
-double arithmetic_avg_to_n(long a[NMAX], size_t n)
+long abs_sum(long a[NMAX], size_t n)
 {
     if (n == 0)
         return 0;
 
-    double sum = 0;
+    long sum = 0;
     for (size_t i = 0; i < n; i++)
-        sum += a[i];
+        sum += labs(a[i]);
 
-    return sum / n;
+    return sum;
 }
 
-size_t delete_by_avg(long a[NMAX], size_t n, double avg)
+long delete_by_sum(long a[NMAX], size_t n, long sum)
 {
     for (size_t i = 0; i < n; i++)
-        if (a[i] > avg)
+        if (a[i] > sum)
             delete_by_i(a, &n, &i);
 
     return n;
@@ -74,8 +74,8 @@ size_t delete_by_avg(long a[NMAX], size_t n, double avg)
 
 void delete_by_i(long a[NMAX], size_t *n, size_t *i)
 {
-    for (size_t l = *i; l < *n - 1; l++)
-        a[l] = a[l + 1];
+    for (size_t k = *i; k < *n - 1; k++)
+        a[k] = a[k + 1];
 
     *n -= 1;
     *i -= 1;
