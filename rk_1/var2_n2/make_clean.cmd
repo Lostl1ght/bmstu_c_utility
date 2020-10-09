de028786b0c420d6b0981b0a64d7a4131e3bcdca
Lostlight <robpirojk@gmail.com>
2020-10-09 20:39:21 +0300

    Repo update

--- rk_1/var2_n2/make_clean.cmd
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-ECHO Cleaning.
-IF EXIST "*.exe" DEL *.exe & ECHO Deleted .exe.
-IF EXIST "*.o" DEL *.o & ECHO Deleted .o.
-IF EXIST "*.gcov" DEL *.gcov & ECHO Deleted .gcov.
-IF EXIST "*.gcda" DEL *.gcda & ECHO Deleted .gcda.
-IF EXIST "*.gcno" DEL *.gcno & ECHO Deleted .gcno.
-ECHO Done.
