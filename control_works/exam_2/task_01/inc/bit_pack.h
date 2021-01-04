c9cf8d826006346b6688a02a2019b2ae7558d6c0
lostlight <robpirojk@gmail.com>
2021-01-05 02:41:12 +0300

    Added bit fields

--- control_works/exam_2/task_01/inc/bit_pack.h
+++ control_works/exam_2/task_01/inc/bit_pack.h
@@ -3,6 +3,14 @@
 
 #include <inttypes.h>
 
+typedef struct
+{
+    unsigned int first : 8;
+    unsigned int second : 8;
+    unsigned int third : 8;
+    unsigned int fourth : 8;
+} four_symb_t;
+
 uint32_t pack_symbols(char a, char b, char c, char d);
 
 void unpack_symbols(uint32_t num, char *a, char *b, char *c, char *d);
--- control_works/exam_2/task_01/src/main.c
+++ control_works/exam_2/task_01/src/main.c
@@ -4,9 +4,16 @@
 int main(void)
 {
     uint32_t num;
-    num = pack_symbols('K', 'E', 'K', 'W');
     char a, b, c, d;
+    num = pack_symbols('K', 'E', 'K', 'W');
     unpack_symbols(num, &a, &b, &c, &d);
-    printf("Symbols: %c %c %c %c", a, b, c, d);
+    printf("Symbols: %c %c %c %c, size: %zu\n", a, b, c, d, sizeof num);
+
+    four_symb_t num1;
+    num1.first = 'K';
+    num1.second = 'E';
+    num1.third = 'K';
+    num1.fourth = 'W';
+    printf("Symbols: %c %c %c %c, size: %zu\n", num1.first, num1.second, num1.third, num1.fourth, sizeof num1);
     return 0;
 }
